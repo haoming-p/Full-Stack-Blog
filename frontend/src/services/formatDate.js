c12fe0220cb281372cf5dde922a40435be47fd2a
haoming <haoming.p@berkeley.edu>
2025-01-05 17:30:28 -0800

    Remove frontend folder to resolve case sensitivity issues

--- frontend/src/services/formatDate.js
+++
@@ -1,8 +0,0 @@
-export function FormatDate(isoString){
-    const date = new Date(isoString)
-    return date.toLocaleString('en-US',{
-        year: 'numeric',
-        month: 'long',
-        day:'2-digit',
-    }).replace(',','')
-}
