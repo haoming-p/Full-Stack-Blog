088960de676d49aa8de95024fc52fcc2f5a380ff
haoming <haoming.p@berkeley.edu>
2025-01-16 13:36:09 -0800

    deploy debug

--- backend/blog_backend/settings.py
+++ backend/blog_backend/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
@@ -111,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR/"static/"
 MEDIA_URL= '/media/'
 MEDIA_ROOT = BASE_DIR /"media"
